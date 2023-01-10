59735ff3e92ca45bf2a0a561106c8572b5573ee4
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2023-01-10 13:13:24 +0530

    commit all

---
+++ client/src/store/apiSlice.js
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const baseURI = "http://localhost:8080";
+
+export const apiSlice = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: baseURI }),
+  endpoints: builder => ({
+    //get categories
+    getCategories: builder.query({
+      //get:"http://localhost:8080"
+      query: () => "/api/categories",
+      providesTags:['categories']
+    }),
+    //get labels
+    getLabels: builder.query({
+      query: () => "/api/labels",
+      providesTags:['transaction']
+
+    }),
+    // add new Transaction
+    addTransaction: builder.mutation({
+      query: (initialTransaction) => ({
+        //post:"http://localhost:8080/api/transaction"
+
+        url: "/api/transaction",
+        method: "POST",
+        body: initialTransaction,
+      }),
+      invalidatesTags:['transaction']
+    }),
+    //delete record
+    deleteTransaction: builder.mutation({
+      query: (recordId) => ({
+        //delete:"http://localhost:8080/api/transaction"
+
+        url: "/api/transaction",
+        method: "DELETE",
+        body: recordId,
+      }),
+      invalidatesTags:['transaction']
+    })
+  })
+});
+
+export default apiSlice;
---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config({path:"./config.env"});
+const port = process.env.PORT||5000 ;
+
+// use middeleware
+
+app.use(cors());
+app.use(express.json());
+
+// mongodb connection
+const con = require('./db/connection.js');
+
+
+//using routes
+
+app.use(require('./routes/route'));
+
+con.then(db=>{
+    if(!db)return process.exit(1);
+
+
+    // listen to the http server
+     
+    app.listen(port,()=>{
+        console.log(`Server is run on port:http://localhost:${port}`)
+    })
+
+    app.on('error',err=>console.log(`Failed to Connect with HTTP server :${err}`));
+    // error in mongodb connection
+}).catch(error=>{
+    console.log(`Connection Failed...!${error}`);
+});
+
